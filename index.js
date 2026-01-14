9a81009d15588b106bef848517e12eb6d2fcbfbe
Kevin Rodriguez <kevindeywin@gmail.com>
2026-01-14 18:41:34 -0500

    chore: configure project & create initial module

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  hello: function() {
+    console.log("Hello, world!");
+  },
+  customHello: function(name) {
+    console.log(`Hello, ${name}!`);
+  },
+  nullaryFunction: function() {
+    console.log("Hello from a nullary function!");
+  }
+}
